1259f4ced28b8d903659f7355f9b38d47cce0c2d
Matej Dujič <matej.dujic@visionect.com>
2018-01-09 14:30:46 +0100

    fixes

--- src/tz/definitions.go
+++ src/tz/definitions.go
@@ -1,12 +1,11 @@
-package tz
+package main
 
 import (
 	"time"
 	"regexp"
-	"errors"
 	"strconv"
-	"koala2/src/golang.org/x/net/html/atom"
 	"bytes"
+	"strings"
 )
 
 type logMessage string
@@ -15,18 +14,18 @@ type timeStamp time.Time
 
 //`[0-9]{4}\/[0-9]{2}\/[0-9]{2}\ [0-9]{2}\:+[0-9]{2}\:+[0-9]{2}\.+[0-9]{6}`
 
-func createRegex(layout string) (string, error) {
+func createRegex(layout string) ([]string) {
 	//regx := `[0-9]{4}\/[0-9]{2}\/[0-9]{2}\ [0-9]{2}\:+[0-9]{2}\:+[0-9]{2}\.+[0-9]{6}`
 	var counters []int
 	var delimiters []string
 	delC := 0
-	for i := 0; i < len(layout); i++ {
-		curS := string(layout[i])
-		if _, err := strconv.Atoi(curS); err == nil {
+	for _, r := range layout {
+		r := string(r)
+		if _, err := strconv.Atoi(r); err == nil {
 			delC++
 		} else {
 			counters = append(counters, delC)
-			delimiters = append(delimiters, curS)
+			delimiters = append(delimiters, r)
 			delC = 0
 		}
 	}
@@ -40,24 +39,25 @@ func createRegex(layout string) (string, error) {
 		} else {
 			regx.WriteString("[0-9]{")
 			regx.WriteString(string(counters[j]))
-			regx.WriteString("}\")
+			regx.WriteString("}\\")
 			regx.WriteString(string(delimiters[j]))
 		}
 	}
+	return strings.Fields(regx.String())
 }
 
 func (l logMessage) getTS(layout string) (timeStamp, error) {
-	rgx, errR := createRegex(layout)
-	if errR != nil {
-		return timeStamp(time.Time{}), errors.New("could not make regex out of given layout")
-	}
+	rgx := createRegex(layout)
+	//if errR != nil {
+	//	return timeStamp(time.Time{}), errors.New("could not make regex out of given layout")
+	//}
 
-	tsReg, _ := regexp.Compile(rgx)
+	tsReg, _ := regexp.Compile(strings.Join(rgx, " "))
 	tsStr := tsReg.FindString(string(l))
 	ts, err := time.Parse(layout, tsStr)
 
 	if err != nil {
-		return timeStamp(time.Time{}), errors.New("could not convert")
+		return timeStamp(time.Time{}), err
 	}
 	return timeStamp(ts), nil
 }
@@ -67,26 +67,9 @@ func (l logMessage) getTS(layout string) (timeStamp, error) {
 func (t timeStamp) changeTo(location string) (timeStamp, error) {
 	loc, err := time.LoadLocation(location)
 	if err == nil {
-		return timeStamp(t.In(loc)), nil
+		return timeStamp(time.Time(t).In(loc)), nil
 	} else {
-		return timeStamp(time.Time{}), errors.New("could not load location")
+		return timeStamp(time.Time{}), err
 	}
-
-
-	//runes := []rune(deviation)
-	//sign := string(runes[0:1])
-	//num, err := strconv.Atoi(string(runes[1:2]))
-	//
-	//if err != nil {
-	//	return time.Time{}, err
-	//}
-	//if sign == "+" {
-	//
-	//} else if sign == "-" {
-	//
-	//} else {
-	//	return time.Time{}, err
-	//}
-
 }
 
--- src/tz/tzParser_test.go
+++ src/tz/tzParser_test.go
@@ -1,12 +1,30 @@
-package tz
+package main
 
 import (
 	"testing"
+	"strings"
 )
 
-func TestTs(t *testing.T) {
+func TestGetTs(t *testing.T) {
 	log := logMessage("2017/12/28 17:10:57.194 UTC (3fcfdf5f.gateway.release_4.2.1.89+cg01961) [info] Didn't find bootloader for '58003000-1351-3432-3434-373300000000', [0 4 0], [3 18 1868], 5")
-	if _, err := log.getTS("2017/01/02 10:04:15.000000"); err != nil {
-		t.Fatal("couldn't get timestamp")
+	if _, err := log.getTS("2006/01/02 10:04:15.999999"); err != nil {
+		t.Fatal("couldn't get timestamp", err)
 	}
+}
+
+func TestChangeTo(t *testing.T) {
+	log := logMessage("2017/12/28 17:10:57.194 UTC (3fcfdf5f.gateway.release_4.2.1.89+cg01961) [info] Didn't find bootloader for '58003000-1351-3432-3434-373300000000', [0 4 0], [3 18 1868], 5")
+	ts, _ := log.getTS("2006/01/02 10:04:15.999999")
+	if _, err := ts.changeTo("Europe/Ljubljana"); err != nil {
+		t.Fatal("couldn't convert", err)
+	}
+}
+
+func TestCreateRegex(t *testing.T) {
+	rgx := createRegex("2006/01/02 10:04:15.999999")
+	rgxP := strings.Join(rgx, " ")
+	if rgxP != "[0-9]{4}\\/[0-9]{2}\\/[0-9]{2}\\ [0-9]{2}\\:+[0-9]{2}\\:+[0-9]{2}\\.+[0-9]{6}" {
+		t.Fatal("couldn't create regexp")
+	}
+
 }
